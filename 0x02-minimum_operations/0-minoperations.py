604fe262fc30eba8c569f8dbc735770e44fbb01f
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-07-12 01:20:49 +0100

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-""" Defines minOperations."""
+"""
+Contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
 
 
 def minOperations(n):
-    """Returns an integer. if n is imposible to achieve, return 0."""
-
-    if not isinstance(n, int):
-        return 0
-    numOperations = 0
-    clipboard = 1
-    done = 1
-    while done < n:
-        if clipboard == 0:
-            clipboard = done
-            done += clipboard
-            numOperations += 2
-        elif n - done > 0 and (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            numOperations += 2
-        elif clipboard > 0:
-            done += clipboard
-            numOperations += 1
-
-    return numOperations
+    """
+    Returns the fewest number of operations needed to result in exactly
+    n H characters in the file.
+    """
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
 
